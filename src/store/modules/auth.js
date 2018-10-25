7558b8cb97888aca86c970b0ec0b49b0c6ff8db3
Alberto Madrazo <otreblatercero@gmail.com>
2018-10-25 06:32:32 -0500

    Avances en el tutorial.

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,5 @@
 import api from '../../api/imgur'; // Cuando sólo hay un export, se puede llamar como lo llamas aquí!?
+import qs from 'qs';
 
 const state = {
 	token: null
@@ -12,6 +13,11 @@ const actions = {
 	login: () => {
 		api.login();
 
+	},
+	finalizeLogin({ commit }, hash) {
+		const query = qs.parse(hash.replace('#', ''));
+
+		commit('setToken', query.access_token);
 	},
 	logout: ({ commit }) => {
 		commit('setToken', null);
